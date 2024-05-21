6b256301e1aacd81b736ef95e80f9cd9fa0bccb7
Tuan Huy <huydo.01012003@gmail.com>
2024-05-21 07:53:12 +0700

    update

---
+++ app/src/main/java/com/example/bai15/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.bai15;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    EditText txtMaLop, txtTenLop, txtSiSo;
+    Button btnThem, btnSua, btnXoa, btnTruyVan;
+    ListView lv;
+    ArrayList<String> mylist;
+    ArrayAdapter<String> myadapter;
+    SQLiteDatabase mydatabase;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtMaLop = findViewById(R.id.txtMaLop);
+        txtTenLop = findViewById(R.id.txtTenLop);
+        txtSiSo = findViewById(R.id.txtSiSo);
+        btnThem = findViewById(R.id.btnThem);
+        btnSua = findViewById(R.id.btnSua);
+        btnXoa = findViewById(R.id.btnXoa);
+        btnTruyVan = findViewById(R.id.btnTruyVan);
+        lv = findViewById(R.id.lv);
+        mylist = new ArrayList<>();
+        myadapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mylist);
+        lv.setAdapter(myadapter);
+
+        mydatabase = openOrCreateDatabase("qlsinhvien.db", MODE_PRIVATE, null);
+        try {
+            String sql = "Create table tbllop(malop text primary key, tenlop text, siso integer)";
+            mydatabase.execSQL(sql);
+        } catch (Exception e) {
+            Log.e("Error","Table da ton tai");
+        }
+    }
+}
