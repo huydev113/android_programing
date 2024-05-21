260071649e6670caf292d96a32b92ce7f7f871f0
Tuan Huy <huydo.01012003@gmail.com>
2024-05-21 09:38:59 +0700

    update bai15

--- app/src/main/java/com/example/bai15/MainActivity.java
+++ app/src/main/java/com/example/bai15/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.bai15;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     EditText txtMaLop, txtTenLop, txtSiSo;
@@ -38,5 +45,129 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             Log.e("Error","Table da ton tai");
         }
+        loadData();
+
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtMaLop.getText().toString().isEmpty() || txtMaLop.getText().toString().isEmpty() || txtSiSo.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Không được để trống", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String malop = txtMaLop.getText().toString();
+                String tenlop = txtTenLop.getText().toString();
+                int siso = Integer.parseInt("0" + txtSiSo.getText().toString());
+                if(siso <= 0) {
+                    Toast.makeText(getApplicationContext(), "Sĩ số phải lớn hơn 0", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Cursor c = mydatabase.query("tbllop", null, "malop=?", new String[]{malop}, null, null, null);
+                if(c.getCount() > 0) {
+                    Toast.makeText(getApplicationContext(), "Mã lớp đã tồn tại", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ContentValues row = new ContentValues();
+                row.put("malop", malop);
+                row.put("tenlop", tenlop);
+                row.put("siso", siso);
+
+                if(mydatabase.insert("tbllop", null, row) != -1) {
+                    Toast.makeText(getApplicationContext(), "Thêm thành công", Toast.LENGTH_SHORT).show();
+                    txtMaLop.setText("");
+                    txtTenLop.setText("");
+                    txtSiSo.setText("");
+                    loadData();
+                }
+
+                else {
+                    Toast.makeText(getApplicationContext(), "Thêm thất bại", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String arr[] = mylist.get(position).split("-");
+                txtMaLop.setText(arr[0]);
+                txtTenLop.setText(arr[1]);
+                txtSiSo.setText(arr[2]);
+            }
+        });
+
+        btnXoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String malop = txtMaLop.getText().toString();
+                if(malop.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Mã lớp không được để trống", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(mydatabase.delete("tbllop", "malop=?", new String[]{malop}) != 0) {
+                    Toast.makeText(getApplicationContext(), "Xóa thành công", Toast.LENGTH_SHORT).show();
+                    txtMaLop.setText("");
+                    txtTenLop.setText("");
+                    txtSiSo.setText("");
+                    loadData();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Xóa thất bại", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnSua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtMaLop.getText().toString().isEmpty() || txtMaLop.getText().toString().isEmpty() || txtSiSo.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Không được để trống", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String malop = txtMaLop.getText().toString();
+                String tenlop = txtTenLop.getText().toString();
+                int siso = Integer.parseInt("0" + txtSiSo.getText().toString());
+                if(siso <= 0) {
+                    Toast.makeText(getApplicationContext(), "Sĩ số phải lớn hơn 0", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                ContentValues row = new ContentValues();
+                row.put("malop", malop);
+                row.put("tenlop", tenlop);
+                row.put("siso", siso);
+
+                if(mydatabase.update("tbllop", row, "malop=?", new String[]{malop}) != 0) {
+                    Toast.makeText(getApplicationContext(), "Sửa thành công", Toast.LENGTH_SHORT).show();
+                    txtMaLop.setText("");
+                    txtTenLop.setText("");
+                    txtSiSo.setText("");
+                    loadData();
+                }
+
+                else {
+                    Toast.makeText(getApplicationContext(), "Sửa thất bại", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnTruyVan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    private void loadData() {
+        mylist.clear();
+        Cursor c = mydatabase.query("tbllop", null, null, null, null, null, null);
+        c.moveToFirst();
+        while (!c.isAfterLast()) {
+            String malop = c.getString(0);
+            String tenlop = c.getString(1);
+            int siso = c.getInt(2);
+            mylist.add(malop + " - " + tenlop + " - " + siso);
+            c.moveToNext();
+        }
+        c.close();
+        myadapter.notifyDataSetChanged();
     }
 }
